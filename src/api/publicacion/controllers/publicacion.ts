ef25a7711a2a4d4d64a46e987724eb18851d05ab
fabrik12 <fbalarezo@unsa.edu.pe>
2025-11-06 16:20:04 -0500

    feat: implement custom find and findOne methods in publicacion controller for enhanced data retrieval

--- src/api/publicacion/controllers/publicacion.ts
+++ src/api/publicacion/controllers/publicacion.ts
@@ -4,4 +4,23 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::publicacion.publicacion');
+export default factories.createCoreController('api::publicacion.publicacion', ({ strapi }) => ({
+    async find(ctx) {
+        const entities  = await strapi.entityService.findMany('api::publicacion.publicacion', {
+            populate: ['categorias'], // Agregar mas relaciones si es necesario
+        });
+        return entities;
+    },
+
+    async findOne(ctx) {
+        const { id } = ctx.params;
+        
+        const entity = await strapi.entityService.findOne('api::publicacion.publicacion', id, {
+            populate: {
+                categorias: true,
+                contenido: true,
+            },
+        });
+        return entity;
+    },
+}));
